268d43149e42b1202afd726fb742124ce93da4fd
srhqmp <ballanosarah@gmail.com>
2024-06-11 00:31:51 +0800

    feat: setup server

---
+++ index.ts
@@ -0,0 +1,16 @@
+import express from "express";
+
+const app = express();
+
+app.use(express.json());
+
+app.get("/api/ping", (_req, res) => {
+  console.log("someone pinged");
+  res.send("pong");
+});
+
+const PORT = 3001;
+
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
